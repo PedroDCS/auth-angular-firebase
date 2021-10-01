8b3304f038f48d23faf261e95f0b6a494928aab9
Pedro Daniel Camargos Soares <PedroDCS@users.noreply.github.com>
2021-10-01 15:57:18 -0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-
+    apiKey: "AIzaSyCtJHE3ETaKT_tMwULInlFBxa6zpGSCf_M",
+    authDomain: "projetos-p.firebaseapp.com",
+    projectId: "projetos-p",
+    storageBucket: "projetos-p.appspot.com",
+    messagingSenderId: "597069773859",
+    appId: "1:597069773859:web:f853398eebdba275810596"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-
+    apiKey: "AIzaSyCtJHE3ETaKT_tMwULInlFBxa6zpGSCf_M",
+    authDomain: "projetos-p.firebaseapp.com",
+    projectId: "projetos-p",
+    storageBucket: "projetos-p.appspot.com",
+    messagingSenderId: "597069773859",
+    appId: "1:597069773859:web:f853398eebdba275810596"
   }
 };
 
